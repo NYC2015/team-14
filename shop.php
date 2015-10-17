c72ffdef40f20a5eb9f1c45bd9bc13c3f334fc54
Michelle Crawley <michelle-crawley@hotmail.com>
2015-10-17 05:29:12 -0400

    preferences changes

--- shop.php
+++ shop.php
@@ -8,18 +8,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
+	
     <title>FoodBuddy</title>
-
+	
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="css/shop-homepage.css" rel="stylesheet">
 
+	<link href="css/toggle.css" rel="stylesheet">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
+		<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.0/css/bootstrap-toggle.min.css" rel="stylesheet">
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
@@ -104,6 +107,15 @@
                                     <span class="glyphicon glyphicon-star"></span>
                                     <span class="glyphicon glyphicon-star"></span>
                                 </p>
+								
+								<div class="onoffswitch">
+									<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
+									<label class="onoffswitch-label" for="myonoffswitch">
+									<span class="onoffswitch-inner"></span>
+									<span class="onoffswitch-switch"></span>
+									</label>
+								</div>
+								
                             </div>
                         </div>
                     </div>
@@ -113,14 +125,12 @@
             </div>
 
         </div>
-
+	
     </div>
-    <!-- /.container -->
+
 
     <div class="container">
-
-        <hr>
-
+        <br>
         <!-- Footer -->
         <footer>
             <div class="row">
